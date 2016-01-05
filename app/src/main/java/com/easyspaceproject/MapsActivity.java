e0f2c9489056b7fad413061e9d4e2c8ab5d24089
Mydjer <djerem02@gmail.com>
2016-01-05 19:16:58 +0100

    -Abonnement aux màj des datas -Arret abonnement

--- app/src/main/java/com/easyspaceproject/MapsActivity.java
+++ app/src/main/java/com/easyspaceproject/MapsActivity.java
@@ -1,5 +1,11 @@
 package com.easyspaceproject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,6 +26,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     static final LatLng INSSET = new LatLng(49.8495161, 3.2874817);
     static final LatLng CAMPUS = new LatLng(49.8374935, 3.3000117);
 
+    private LocationManager locationManager;
+    private Marker maPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +40,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     }
-
-    
 
 
     /**
@@ -49,10 +56,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         //Zoom sur ...
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CAMPUS, 4));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(13),3000,null);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13), 3000, null);
 
         //Marqueurs
         final Marker insset = mMap.addMarker(new MarkerOptions().position(INSSET).title("Aurevoir le centre ville !"));
         final Marker campus = mMap.addMarker(new MarkerOptions().position(CAMPUS).title("Notre nouveau campus !"));
+        maPosition = mMap.addMarker(new MarkerOptions().title("Vous êtes ici !").position(new LatLng(0, 0)));
     }
+
+    //Localisation
+    @Override
+    /*Mise à jour des coordonnées*/
+    public void onResume() {
+        super.onResume();
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, (LocationListener) this);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, (LocationListener) this);
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+            }
+
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationManager.removeUpdates((LocationListener) this);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+        }
+
+
+    }
+
+    /*Quand une source est activée */
+
+
+
+
+
+
+
 }
